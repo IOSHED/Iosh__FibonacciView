2bf8fc6d2cb0c8a1b3d49520768ecd1cbd1c1b7a
IOSHED <iveninvala7@gmail.com>
2025-06-16 11:38:03 +0300

    Feat: Add icon for exe file.

---
+++ crates/fibo_view/build.rs
@@ -0,0 +1,13 @@
+extern crate winres;
+
+const PATH_TO_ICON: &str = "../../resources/logo.ico";
+
+fn main() {
+    let mut res = winres::WindowsResource::new();
+
+    res.set_icon(PATH_TO_ICON);
+    res.set("FileDescription", "Calculator for Fibonacci numbers");
+    res.set("ProductName", "Fibonacci View");
+
+    res.compile().unwrap();
+}
