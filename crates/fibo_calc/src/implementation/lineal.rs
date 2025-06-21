07d44179098b6bc9dec2092adb6e69d6ffbaca84
IOSHED <iveninvala7@gmail.com>
2025-06-21 19:38:56 +0300

    Feat: Add test for `lineal.rs`.

--- crates/fibo_calc/src/implementation/lineal.rs
+++ crates/fibo_calc/src/implementation/lineal.rs
@@ -36,3 +36,54 @@ impl Iterator for LinealFibo {
         Some(res)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use num_bigint::BigInt;
+    use test_case::test_case;
+
+    #[test]
+    fn test_default_values() {
+        let fibo = LinealFibo::default();
+        assert_eq!(fibo.pre_last, BigInt::from(-1));
+        assert_eq!(fibo.last, BigInt::from(1));
+    }
+
+    #[test_case((BigInt::from(0), BigInt::from(1)), 1, 1; "classic start")]
+    #[test_case((BigInt::from(2), BigInt::from(3)), 3, 5; "custom start")]
+    #[test_case((BigInt::from(-5), BigInt::from(8)), 8, 3; "negative start")]
+    fn test_new_with_start_nums(start: (BigInt, BigInt), expected_last: i32, expected_next: i32) {
+        let mut fibo = LinealFibo::new(Some((start.0.clone(), start.1.clone())));
+        assert_eq!(fibo.pre_last, start.0);
+        assert_eq!(fibo.last, start.1);
+        let next = fibo.next().unwrap();
+        assert_eq!(
+            next,
+            BigInt::from(expected_last + expected_next - expected_last)
+        );
+    }
+
+    #[test]
+    fn test_sequence() {
+        let mut fibo = LinealFibo::new(Some((BigInt::from(0), BigInt::from(1))));
+        let expected = vec![1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+        for val in expected {
+            assert_eq!(fibo.next().unwrap(), BigInt::from(val));
+        }
+    }
+
+    #[test]
+    fn test_iterator_trait() {
+        let fibo = LinealFibo::default();
+        let vals: Vec<BigInt> = fibo.take(5).collect();
+        let expected = vec![
+            BigInt::from(0),
+            BigInt::from(1),
+            BigInt::from(1),
+            BigInt::from(2),
+            BigInt::from(3),
+        ];
+        assert_eq!(vals, expected);
+    }
+}
